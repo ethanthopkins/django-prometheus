261d4a078bec6843edbfd3850e89222d04954fb3
Ethan <72623865+ethanthopkins@users.noreply.github.com>
2026-06-01 15:21:07 -0600

    Renames django info to django version for more clarity.

--- django_prometheus/info.py
+++ django_prometheus/info.py
@@ -2,7 +2,7 @@ from django import get_version
 from prometheus_client import Info
 from django_prometheus.conf import NAMESPACE
 
-django_info = Info("django_info", 
+django_info = Info("django_version", 
                    "Django version information", 
                    namespace=NAMESPACE)
 django_info.info({"version": get_version()})
