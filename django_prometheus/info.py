ddc4ac9083faf525258e92ca43153f3f510dba0f
Ethan <72623865+ethanthopkins@users.noreply.github.com>
2026-06-03 11:38:31 -0600

    Adds major, minor, and patch to Django version metric. Also changes name to "Django" because "_info" is appended already in the metrics endpoint and for consistency with the Python_info metric. Updates unit test to reflect changes.

--- django_prometheus/info.py
+++ django_prometheus/info.py
@@ -2,7 +2,10 @@ from django import get_version
 from prometheus_client import Info
 from django_prometheus.conf import NAMESPACE
 
-django_info = Info("django_version", 
+django_info = Info("django", 
                    "Django version information", 
                    namespace=NAMESPACE)
-django_info.info({"version": get_version()})
+django_info.info({"major": get_version().split('.')[0], 
+                  "minor": get_version().split('.')[1], 
+                  "patchlevel": get_version().split('.')[2], 
+                  "version": get_version()})
--- django_prometheus/tests/test_info.py
+++ django_prometheus/tests/test_info.py
@@ -2,6 +2,9 @@ from django import get_version
 
 def test_info_metric():
     from django_prometheus.info import django_info
-    assert django_info._name == "django_info"
+    assert django_info._name == "django"
     assert django_info._documentation == "Django version information"
-    assert django_info._value == {"version": get_version()}
+    assert django_info._value == {"major": get_version().split('.')[0], 
+                                  "minor": get_version().split('.')[1], 
+                                  "patchlevel": get_version().split('.')[2], 
+                                  "version": get_version()}
