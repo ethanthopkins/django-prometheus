096efa4a156ad21b2a1f22372176e9deba6e6aba
Ethan <72623865+ethanthopkins@users.noreply.github.com>
2026-06-03 13:12:36 -0600

    Uses VERSION to retrieve the major, minor, and patch so it is less brittle.

--- django_prometheus/info.py
+++ django_prometheus/info.py
@@ -1,11 +1,13 @@
-from django import get_version
+from django import VERSION, get_version
 from prometheus_client import Info
 from django_prometheus.conf import NAMESPACE
 
+major, minor, patch = VERSION[:3]
+version = get_version()
 django_info = Info("django", 
                    "Django version information", 
                    namespace=NAMESPACE)
-django_info.info({"major": get_version().split('.')[0], 
-                  "minor": get_version().split('.')[1], 
-                  "patchlevel": get_version().split('.')[2], 
-                  "version": get_version()})
+django_info.info({"major": str(major), 
+                  "minor": str(minor), 
+                  "patchlevel": str(patch), 
+                  "version": version})
