163dcd0e92a688b231557b50f9ed9cf5f35ae24e
Ethan <72623865+ethanthopkins@users.noreply.github.com>
2026-06-03 13:17:33 -0600

    Updates unit test to be less brittle.

--- django_prometheus/tests/test_info.py
+++ django_prometheus/tests/test_info.py
@@ -1,10 +1,12 @@
-from django import get_version
+from django import VERSION, get_version
 from django_prometheus.info import django_info
 
 def test_info_metric():
+    major, minor, patch = VERSION[:3]
     assert django_info._name == "django"
     assert django_info._documentation == "Django version information"
-    assert django_info._value == {"major": get_version().split('.')[0], 
-                                  "minor": get_version().split('.')[1], 
-                                  "patchlevel": get_version().split('.')[2], 
-                                  "version": get_version()}
+    assert django_info._value == {"major": str(major), 
+                                  "minor": str(minor), 
+                                  "patchlevel": str(patch), 
+                                  "version": get_version(),
+                                  }
